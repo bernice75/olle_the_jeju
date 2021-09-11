9c83f808c122426519a88f75f33fa81174eaea3a
InJungPark <in2066@naver.com>
2021-09-11 22:11:37 +0900

    회원가입

--- OlleTheJeju/src/main/java/com/olle/controller/MemberController.java
+++ OlleTheJeju/src/main/java/com/olle/controller/MemberController.java
@@ -1,15 +1,42 @@
 package com.olle.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.olle.biz.member.MemberBiz;
+import com.olle.dto.member.MemberDto;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller
 public class MemberController {
 	
-	@RequestMapping(value = "member_list.do", method = RequestMethod.GET)
-	public String member_list() {
-		return "page_/";
+	private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
+
+	@Autowired
+	private MemberBiz memberBiz;
+	
+	/**
+	 * @param dto
+	 * @return
+	 * 회원 정보를 저장한다.
+	 */
+	@RequestMapping(value="userInsert.do")
+	@ResponseBody
+	public String userInsert(MemberDto dto) {
+		
+		System.out.println("dto");
+		System.out.println(dto);		
+		
+		memberBiz.userInsert(dto);	// 회원 정보를 저장한다.
+		
+		return "home.do";
 	}
 	
 }
