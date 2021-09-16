908d4bc74da654c9e76bbc93df3127c6920079ff
JHG <qwwe1321@gmail.com>
2021-09-16 17:56:41 +0900

    1

---
+++ OlleTheJeju/src/main/java/com/olle/biz/trip/TripBiz.java
@@ -0,0 +1,18 @@
+package com.olle.biz.trip;
+
+import java.util.List;
+import java.util.Map;
+
+import com.olle.dto.trip.TripDto;
+
+public interface TripBiz {
+	public Map<String, List> getJeju(String page);
+	public Map<String, List> getSearch(String search);
+	public int insert(TripDto dto);
+	public List<TripDto> selectList(String kategorie, int page);
+	public int getAllCount(String kategorie);
+	public TripDto selectOne(int trip_num);
+	public int update(TripDto dto);
+	public int delete(int trip_num);
+	public void likeUpdate(int trip_num);
+}
