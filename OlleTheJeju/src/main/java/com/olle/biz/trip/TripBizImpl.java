36d686ced9b4e6d90fd4c06482f5e75c1457cd92
JHG <qwwe1321@gmail.com>
2021-09-16 17:54:36 +0900

    파일 수정

--- OlleTheJeju/src/main/java/com/olle/biz/trip/TripBizImpl.java
+++ OlleTheJeju/src/main/java/com/olle/biz/trip/TripBizImpl.java
@@ -6,36 +6,37 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.olle.dao.trip.TripDaoImpl;
-import com.olle.dao.trip.Paging;
+import com.olle.dao.trip.TripDao;
 import com.olle.dto.trip.TripDto;
 
 @Service
-public class TripBiz {
+public class TripBizImpl implements TripBiz{
 	
 	@Autowired
-	private TripDaoImpl td;
+	private TripDao td;
 	
-	
+	@Override
 	public Map<String, List> getJeju(String page) {
 		Map<String, List> result = td.getJeju(page);
 		
 		return result;
 	}
 
-	
+	@Override
 	public Map<String, List> getSearch(String search) {
 		Map<String, List> result = td.getSearch(search);
 		
 		return result;
 	}
 	
+	@Override
 	public int insert(TripDto dto) {
 		int res = td.insert(dto);
 		
 		return res;
 	}
 	
+	@Override
 	public List<TripDto> selectList(String kategorie, int page) {
 		
 		List<TripDto> result = td.selectList(kategorie, page);
@@ -43,22 +44,27 @@ public class TripBiz {
 		return result;
 	}
 	
+	@Override
 	public int getAllCount(String kategorie) {
 		return td.getAllCount(kategorie);
 	}
 	
+	@Override
 	public TripDto selectOne(int trip_num) {
 		return td.selectOne(trip_num);
 	}
 	
+	@Override
 	public int update(TripDto dto) {
 		return td.update(dto);
 	}
 	
+	@Override
 	public int delete(int trip_num) {
 		return td.delete(trip_num);
 	}
 	
+	@Override
 	public void likeUpdate(int trip_num) {
 		td.likeUpdate(trip_num);
 	}
