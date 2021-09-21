06dcf77917751df378634d0782362f3cc4a3ec68
JSJeong <hy6219@naver.com>
2021-09-21 10:00:09 +0900

    fix:merge conflict

--- OlleTheJeju/src/main/java/com/olle/dao/etc/ImgDao.java
+++ OlleTheJeju/src/main/java/com/olle/dao/etc/ImgDao.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.olle.dao.etc;
 
 import java.util.List;
@@ -29,34 +28,4 @@ public interface ImgDao {
 	public int cusInsert(ImgDto dto);
 	public List<ImgDto> selectList(int board_num);
 }
-=======
-package com.olle.dao.etc;
 
-import java.util.List;
-
-import com.olle.dto.etc.ImgDto;
-
-public interface ImgDao {
-	String NAMESPACE = "img.";
-
-	public List<ImgDto> selectList();
-	public ImgDto selectOne(int img_num);
-	public int insert(ImgDto dto);
-	public int update(ImgDto dto);
-	public int delete(int img_num);
-	
-	//이미지 최대 PK값 가져오기
-	int selectMaxPK();
-	//group num 최댓값 가져오기
-	int selectMaxGroupId(int boardNum);
-	
-	int saveStoreImg(ImgDto dto);
-	
-	//ImgDto getStoreImg(int table_num);
-	List<ImgDto> getStoreImg(int startIdx, int endIdx);
-
-	//나만의 일정 이미지 저장
-	public int cusInsert(ImgDto dto);
-	public List<ImgDto> selectList(int board_num);
-}
->>>>>>> main
