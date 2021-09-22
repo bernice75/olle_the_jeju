b7ace5b854e26b994cb45937dc9109abf1e00df7
olleTheJeju <89083892+olleTheJeju@users.noreply.github.com>
2021-09-22 14:32:31 +0900

    Update NoticeDao.java

--- OlleTheJeju/src/main/java/com/olle/dao/notice/NoticeDao.java
+++ OlleTheJeju/src/main/java/com/olle/dao/notice/NoticeDao.java
@@ -1,5 +1,12 @@
 package com.olle.dao.notice;
 
-public class NoticeDao {
+import java.util.List;
 
+import com.olle.dto.faq.FaqDto;
+
+public interface NoticeDao {
+	String NAMESPACE = "faq.";
+	
+	public List<FaqDto> faqList();
+	public int insert(FaqDto dto);
 }
