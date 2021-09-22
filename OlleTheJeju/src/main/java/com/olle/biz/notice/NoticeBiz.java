d4e9ed73e480b8bd115db1ef88389027216e0d04
olleTheJeju <89083892+olleTheJeju@users.noreply.github.com>
2021-09-22 14:30:31 +0900

    Update NoticeBiz.java

--- OlleTheJeju/src/main/java/com/olle/biz/notice/NoticeBiz.java
+++ OlleTheJeju/src/main/java/com/olle/biz/notice/NoticeBiz.java
@@ -1,5 +1,10 @@
 package com.olle.biz.notice;
 
-public class NoticeBiz {
+import java.util.List;
 
+import com.olle.dto.faq.FaqDto;
+
+public interface NoticeBiz {
+	public List<FaqDto> faqList();
+	public int insert(FaqDto dto);
 }
