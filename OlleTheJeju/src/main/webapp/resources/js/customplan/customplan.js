f8eaa167fb2a970490c1e800d09425287b9c70a8
olleTheJeju <89083892+olleTheJeju@users.noreply.github.com>
2021-09-23 21:55:19 +0900

    Delete OlleTheJeju/src/main/webapp/resources directory

--- OlleTheJeju/src/main/webapp/resources/js/customplan/customplan.js
+++
@@ -1,3 +0,0 @@
-function search() {
-	location.href="customplan_main.do?search=" + $(".search1").val() + "&page=1";
-}
--- OlleTheJeju/src/main/webapp/resources/js/jejusituation/jejusituation_corona.js
+++
@@ -1,9 +0,0 @@
-var myModal=document.querySelector(".my-modal");
-
-function openModal(){
-    myModal.style.display="flex";
-}
-
-function closeModal(){
-    myModal.style.display="none";
-}
